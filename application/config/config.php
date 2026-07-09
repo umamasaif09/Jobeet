569516377ae002f1f3dbf5731b8cbb058e8b2b99
umamasaif09 <umama.saif@jintech.com>
2026-07-09 16:44:58 +0500

    Routes: use routes for clean urls

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://jobeet.test/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = ''; //removed index.php
 
 /*
 |--------------------------------------------------------------------------
