ff0d8005a095458d3d24d2e2a6a60623b1562bea
umamasaif09 <umama.saif@jintech.com>
2026-07-10 10:43:52 +0500

    Update: update admin menu

--- application/views/partials/adminHeader.php
+++ application/views/partials/adminHeader.php
@@ -1,21 +1,20 @@
 <?php $active = isset($active)? $active: ''; ?>
-
-<div class="container">
-    <header class = "admin-title">
-    <nav class = "admin-nav">
-        <a href="<?php echo site_url('admin/dashboard'); ?>" class="nav-item <?php echo($active == 'dashboard')? 'active' : ''; ?>">
-            <?= $this->lang->line("dashboard"); ?>
-        </a>
-        <a href="<?php echo site_url("admin/categories"); ?>" class="nav-item <?php echo($active == 'categories')? 'active' : ''; ?>">
-            <?= $this->lang->line("manage_categories"); ?>
-        </a>
-        <a href="<?php echo site_url("admin/jobs"); ?>" class="nav-item <?php echo($active == 'jobs')? 'active' : ''; ?>">
-            <?= $this->lang->line("manage_jobs"); ?>
-        </a>
-        <a href="<?php echo site_url("admin/affiliates"); ?>" class="nav-item <?php echo($active == 'affiliates')? 'active' : ''; ?>">
-            <?= $this->lang->line("manage_affiliates"); ?>
-        </a>
+<div class="admin-header">
+    <div class="container">
+        <nav class = "admin-nav">
+            <a href="<?php echo site_url('admin/dashboard'); ?>" class="nav-item <?php echo($active == 'dashboard')? 'active' : ''; ?>">
+                <?= $this->lang->line("dashboard"); ?>
+            </a>
+            <a href="<?php echo site_url("admin/categories"); ?>" class="nav-item <?php echo($active == 'categories')? 'active' : ''; ?>">
+                <?= $this->lang->line("manage_categories"); ?>
+            </a>
+            <a href="<?php echo site_url("admin/jobs"); ?>" class="nav-item <?php echo($active == 'jobs')? 'active' : ''; ?>">
+                <?= $this->lang->line("manage_jobs"); ?>
+            </a>
+            <a href="<?php echo site_url("admin/affiliates"); ?>" class="nav-item <?php echo($active == 'affiliates')? 'active' : ''; ?>">
+                <?= $this->lang->line("manage_affiliates"); ?>
+            </a>
         
     </nav>
-</header>
+    </div>
 </div>
