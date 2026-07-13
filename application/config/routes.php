1a3513422d2cd4080c9ff8015e366242d89c8281
umamasaif09 <umama.saif@jintech.com>
2026-07-13 12:50:55 +0500

    Update: UI consistencies and bug fixes

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,4 +53,4 @@ $route['default_controller'] = 'jobs';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route["admin"] = "admin";
+$route["admin"] = "admin/dashboard";
--- application/views/affiliates/apply.php
+++ application/views/affiliates/apply.php
@@ -1,5 +1,5 @@
 
-    <form method="POST" action="<?php echo site_url("affiliates/submitApplication"); ?>">
+    <form method="POST" action="<?php echo site_url("affiliates/submitApplication"); ?>" class="job-form">
         <?php $this->load->view("partials/affiliates_form_fields"); ?>
         <button type="submit" class="btn btn-primary"><?= $this->lang->line("apply"); ?></button>
     </form>
